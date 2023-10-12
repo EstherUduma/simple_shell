3c532a92ab348a2520fc5fb50225ddbaca3e1caa
EstherUduma <estheruduma23@gmail.com>
2023-10-12 02:23:38 -0700

    errors have not been fixed

--- shell.h
+++ shell.h
@@ -17,5 +17,10 @@ void *custom_memcpy(void *dst, const void *src, size_t n);
 int customStringCopy(char *destination, const char *source, int size);
 int customStringLength(const char *str);
 int customStringCompare(const char *strA, const char *strB);
+char *customGetEnv(const char *name);
+char *custom_strdup(const char *input_str);
+char *custom_strcat(char *destination, const char *source);
+int custom_strncmp(const char *s1, const char *s2, size_t n);
+char *customWhich(const char *command);
 
 #endif
