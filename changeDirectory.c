ec14e3f5147288c67f9da74518eea106949aef69
EstherUduma <estheruduma23@gmail.com>
2023-10-16 02:52:26 -0700

    changeDirectory.c

---
+++ changeDirectory.c
@@ -0,0 +1,49 @@
+#include "shell.h"
+
+/**
+* changeDirectory - function changes the current working directory
+* @newPath: the path to change
+* Return: nothing
+*/
+
+void changeDirectory(char *newPath)
+{
+	char *homeDir = customGetEnv("HOME");
+	char *oldWorkingDir = customGetEnv("OLDPWD");
+	char *currentWorkingDir = customGetEnv("PWD"), currentDit[1024];
+
+	if (newPath == NULL)
+	{
+		if (chdir(homeDir) == -1)
+			perror("cd");
+		else
+		{
+			customSetEnv("OLDPWD", currentWorkingDir, 1);
+			customSetEnv("PWD", homeDir, 1);
+		}
+	}
+	else if (customStringCompare(newPath, "-") == 0)
+	{
+		if (oldWorkingDir != NULL)
+		{
+			if (chdir(oldWorkingDir) == -1)
+				perror("cd")
+			else
+			{
+				customSetEnv("OLDPWD", currentWorkingDir, 1);
+				customSetEnv("PWD", oldWorkingDir, 1);
+			}
+		}
+	}
+	else
+	{
+		if (chdir(newPath) == -1)
+			perror("cd");
+		else
+		{
+			getcwd(currentDir, sizeof(currentDir));
+			customSetEnv("OLDPWD", currentWorkingDir, 1);
+			customSetEnv("PWD", currentDir, 1);
+		}
+	}
+}
